1eb587a161870934c860204a4a788f4be7ec5e02
niels voogt <n.voogt@gmail.com>
2023-08-08 15:43:17 +0200

    Added upload functionality with uploadthings

--- components/shared/AccountProfile.tsx
+++ components/shared/AccountProfile.tsx
@@ -6,7 +6,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { UserValidation } from "@/lib/validations/user";
 
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import Image from "next/image";
 
 import {
@@ -22,38 +22,72 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from "@/lib/uploadthing";
+
 type AccountProfileProps = {
   user: {
     id: string;
     objectId: string;
+    name: string;
+    image: string;
     username: string;
     bio: string;
-    image: string;
   };
   btnTitle: string;
 };
 
 const AccountProfile = ({ user, btnTitle }: AccountProfileProps) => {
+  const [files, setFiles] = useState<File[]>([]);
+  const { startUpload } = useUploadThing("media");
+
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
-      profile_photo: "",
-      name: "",
-      username: "",
-      bio: "",
+      profile_photo: user?.image || "",
+      name: user?.name || "",
+      username: user?.username || "",
+      bio: user?.bio || "",
     },
   });
 
   const handleImage = (
-    e: ChangeEvent,
+    e: ChangeEvent<HTMLInputElement>,
     fieldChange: (value: string) => void
   ) => {
-    console.log(e);
     e.preventDefault();
+
+    const fileReader = new FileReader();
+
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      setFiles(Array.from(e.target.files));
+
+      if (!file.type.includes("image")) return;
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event.target?.result?.toString() || "";
+
+        fieldChange(imageDataUrl);
+      };
+
+      fileReader.readAsDataURL(file);
+    }
+
+    // TODO: update user profile
   };
 
-  const onSubmit = (values: z.infer<typeof UserValidation>) => {
-    console.log("hello, I submitted", values);
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+    const blob = values.profile_photo;
+    const hasImageChanged = isBase64Image(blob);
+
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files);
+
+      if (imgRes && imgRes[0].fileUrl) {
+        values.profile_photo = imgRes[0].fileUrl;
+      }
+    }
   };
 
   return (
@@ -110,7 +144,7 @@ const AccountProfile = ({ user, btnTitle }: AccountProfileProps) => {
               <FormLabel className="text-base-semibold text-light-2">
                 Name
               </FormLabel>
-              <FormControl className="flex-1 text-base-semibold text-gray-200">
+              <FormControl>
                 <Input
                   type="text"
                   className="account-form_input no-focus"
@@ -131,7 +165,7 @@ const AccountProfile = ({ user, btnTitle }: AccountProfileProps) => {
               <FormLabel className="text-base-semibold text-light-2">
                 Username
               </FormLabel>
-              <FormControl className="flex-1 text-base-semibold text-gray-200">
+              <FormControl>
                 <Input
                   type="text"
                   className="account-form_input no-focus"
@@ -152,8 +186,12 @@ const AccountProfile = ({ user, btnTitle }: AccountProfileProps) => {
               <FormLabel className="text-base-semibold text-light-2">
                 Bio
               </FormLabel>
-              <FormControl className="flex-1 text-base-semibold text-gray-200">
-                <Textarea className="account-form_input no-focus" {...field} />
+              <FormControl>
+                <Textarea
+                  rows={6}
+                  className="account-form_input no-focus"
+                  {...field}
+                />
               </FormControl>
             </FormItem>
           )}
